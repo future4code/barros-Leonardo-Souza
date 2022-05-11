9fe1ac12cd89a1aa4f070ea4440fe4b3a688f9e9
Leonardo Barros de Souza <barrosdesouzaleonardo@gmail.com>
2022-05-10 21:40:26 -0300

    Adicionar exercicio 09

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -101,6 +101,14 @@ function retornaNPrimeirosPares(n) {
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
+    if (ladoA === ladoB && ladoB === ladoC) {
+        return "Equilátero"
+    } else if(ladoA !== ladoB && ladoB !== ladoC && ladoA !== ladoC) {  
+        return "Escaleno"
+    } else {
+        return "Isósceles"
+    }
+    
 
 }
 
