b884a7ffdcffff9fd83f9949b79ed8b1473ea6ba
Leonardo <barrosdesouzaleonardo@gmail.com>
2022-10-14 15:34:44 -0300

    adicionar intro typescript

---
+++ modulo5/intro-typescript/src/exercicio2.ts
@@ -0,0 +1,13 @@
+const color1 = process.argv[2]
+const color2 = process.argv[3]
+const color3 = process.argv[4]
+
+
+
+
+function imprimeTresCoresFavoritas(col1: string, col2:string, col3:string): void {
+    console.log([col1, col2, col3])
+ }
+
+
+  imprimeTresCoresFavoritas(color1, color2, color3)
---
+++ modulo5/intro-typescript/src/exercicio4.ts
@@ -0,0 +1,23 @@
+const num1 = Number(process.argv[2])
+const num2 = Number(process.argv[3])
+
+
+function comparaDoisNumeros(num1:number, num2:number): number {
+    let maiorNumero;
+    let menorNumero;
+  
+    if (num1 > num2) {
+      maiorNumero = num1;
+      menorNumero = num2;
+    } else {
+      maiorNumero = num2;
+      menorNumero = num1;
+    }
+  
+    const diferenca = maiorNumero - menorNumero;
+  
+    return diferenca 
+  }
+
+  console.log(comparaDoisNumeros(num1, num2));
+  
