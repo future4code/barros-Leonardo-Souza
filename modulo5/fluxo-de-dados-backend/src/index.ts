27d7bbc1adf336bf732bbc5b2ec526943a6992ad
Leonardo Barros de Souza <leonardo.bsouza@hotmail.com>
2022-11-02 11:28:36 -0300

    Adicionar até o exercicio 7

--- modulo5/fluxo-de-dados-backend/src/index.ts
+++ modulo5/fluxo-de-dados-backend/src/index.ts
@@ -1,3 +1,9 @@
+import { generateId } from "./functions";
+
+import { productsType } from "./type";
+
+import { products } from "./data";
+
 import express from "express";
 
 import cors from "cors";
@@ -12,19 +18,79 @@ app.listen(3003, () => {
   console.log("Server is running in http://localhost:3003");
 });
 
-
 // EXERCICIO 1
-app.get("/test", (req:express.Request, res:express.Response) => {
-    res.status(200).send("Server is running in http://localhost:3003");
-})
+app.get("/test", (req: express.Request, res: express.Response) => {
+  res.status(200).send("Server is running in http://localhost:3003");
+});
 
 // EXERCICIO 4
 
-app.post("/newtask", (req:express.Request, res:express.Response) => {
-    const body = req.body;
-    try {
-        
-    } catch (error) {
-        
+app.post("/newproduct", (req: express.Request, res: express.Response) => {
+  const body = req.body;
+  const checkName = typeof body.name;
+  const newId = generateId(10);
+  const newProduct = {
+    id: newId,
+    name: body.name,
+    price: body.price,
+  };
+  try {
+    if (!body) {
+      throw new Error("Missing body");
+    } else if (!body.name) {
+      throw new Error("Missing name");
+    } else if (!body.price) {
+      throw new Error("Missing price");
     }
-}) 
+    products.push(newProduct);
+    res.status(201).send(products);
+  } catch (err: any) {
+    res.status(400).send(err.message);
+  }
+});
+
+// EXERCICIO 5
+
+app.get("/getallproducts", (req: express.Request, res: express.Response) => {
+  res.status(200).send(products);
+});
+
+// EXERCICIO 6
+
+app.put("/editprice", (req: express.Request, res: express.Response) => {
+  const body = req.body.price;
+  const id = req.query.id;
+  try {
+    if (!body) {
+      throw new Error("Body is required");
+    } else if (!id) {
+      throw new Error("Id is required");
+    }
+    const findProduct = products.filter((element) => {
+      return element.id === id;
+    });
+    findProduct[0].price = body;
+    res.send(findProduct);
+  } catch (err: any) {
+    res.status(400).send(err.message);
+  }
+});
+
+// EXERCICIO 7
+
+app.delete("/deleteproduct",(req: express.Request, res: express.Response) => {
+  const id = req.query.id;
+  try {
+    if (!id) {
+      throw new Error("Id is required");
+    }
+    const filteredProducts = products.filter((element) => {
+      return element.id !== id;
+    })
+    res.status(200).send(filteredProducts)
+  } catch (error:any) {
+    res.status(400).send(error.message)
+  }
+})
+
+// EXERCICIO 8
