56f2914ea88bca7b774e19409cc36afc8f4dd47d
Leonardo Barros de Souza <leonardo.bsouza@hotmail.com>
2022-11-02 11:53:42 -0300

    adicionar exercicio completo

--- modulo5/fluxo-de-dados-backend/src/index.ts
+++ modulo5/fluxo-de-dados-backend/src/index.ts
@@ -29,18 +29,22 @@ app.post("/newproduct", (req: express.Request, res: express.Response) => {
   const body = req.body;
   const checkName = typeof body.name;
   const newId = generateId(10);
+  let string = typeof "string";
   const newProduct = {
     id: newId,
     name: body.name,
     price: body.price,
   };
+  // EXERCICIO 8
   try {
     if (!body) {
       throw new Error("Missing body");
-    } else if (!body.name) {
-      throw new Error("Missing name");
-    } else if (!body.price) {
-      throw new Error("Missing price");
+    } else if (!body.name || typeof body.name !== string) {
+      throw new Error("Missing name or name is not a string");
+    } else if (body.price <= 0) {
+      throw new Error("Price must be greater than zero");
+    } else if (!body.price || isNaN(body.price)) {
+      throw new Error("Missing price or price is not a number");
     }
     products.push(newProduct);
     res.status(201).send(products);
@@ -60,11 +64,18 @@ app.get("/getallproducts", (req: express.Request, res: express.Response) => {
 app.put("/editprice", (req: express.Request, res: express.Response) => {
   const body = req.body.price;
   const id = req.query.id;
+
+  // EXERCICIO 9
+
   try {
-    if (!body) {
+    if (body <= 0) {
+      throw new Error("Price must be greater than zero");
+    } else if (!body) {
       throw new Error("Body is required");
     } else if (!id) {
       throw new Error("Id is required");
+    } else if (isNaN(body)) {
+      throw new Error("Price is not a number");
     }
     const findProduct = products.filter((element) => {
       return element.id === id;
@@ -78,19 +89,23 @@ app.put("/editprice", (req: express.Request, res: express.Response) => {
 
 // EXERCICIO 7
 
-app.delete("/deleteproduct",(req: express.Request, res: express.Response) => {
+app.delete("/deleteproduct", (req: express.Request, res: express.Response) => {
   const id = req.query.id;
+  // EXERCICIO 10
   try {
+    const checkIfProductExist = products.filter((element) => {
+      return element.id === id;
+    });
     if (!id) {
       throw new Error("Id is required");
+    } else if (checkIfProductExist.length === 0) {
+      throw new Error("Product not found");
     }
     const filteredProducts = products.filter((element) => {
       return element.id !== id;
-    })
-    res.status(200).send(filteredProducts)
-  } catch (error:any) {
-    res.status(400).send(error.message)
+    });
+    res.status(200).send(filteredProducts);
+  } catch (error: any) {
+    res.status(400).send(error.message);
   }
-})
-
-// EXERCICIO 8
+});
